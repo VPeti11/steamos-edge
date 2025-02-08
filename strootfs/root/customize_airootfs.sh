2ec98053e51a54c49ebb5fc2b434d188088573ea
VPeti1 <vpeti011@protonmail.com>
2025-02-08 14:29:08 +0100

    Fixed password errors and increased COWSPACE

--- strootfs/root/customize_airootfs.sh
+++ strootfs/root/customize_airootfs.sh
@@ -2,4 +2,7 @@
 chmod +x /home/deck/Desktop/*
 chmod +x /home/deck/tools/*
 systemctl enable sddm
+useradd -M deck -g deck
+chown -R deck /home/deck
+echo -e "deck\ndeck" | sudo passwd deck
 systemctl enable /etc/systemd/system/pr.service # Maybe this works
