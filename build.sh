356cbc0accd06c1324a83f2e97aacdb0dcd0c279
Mohamed <mohamed.o.ashraf173@gmail.com>
2025-02-09 23:12:06 +0400

    Update build system

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
 set -e
 
 if command -v pacman &> /dev/null
@@ -11,3 +12,21 @@ fi
 
 sudo chmod +x ./mksteamos
 sudo ./mksteamos -v ./
+
+# Legacy Stuff...
+#export maker=mksteamos
+#if `$1` == `mksteamos`; then
+#    export maker=mksteamos
+#else
+#    export maker=mkarchiso
+#fi
+#
+#if `$2` == `--x86`; then
+#    echo "Building for x86"
+#    rm -rf profiledef.sh
+#    mv profiledef_x86.sh.template profiledef.sh
+#else
+#     echo "Building for x86_64"
+#fi
+#
+#$maker -v -w build/ -o build/ .
